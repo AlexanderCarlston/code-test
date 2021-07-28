19e31042b6d7cd0edd895c69b8f34a310584ba27
AlexanderCarlston <alekscarlston@gmail.com>
2021-07-28 11:06:52 -0600

    Starting project

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require( "express" );
+const app = express();
+const port = 8080; // default port to listen
+const { Octokit } = require("@octokit/core");
+const octokit = new Octokit();
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    res.send( "Hello world!" );
+} );
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
+
+
+async function test() {
+  let response = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
+    owner: 'octocat',
+    repo: 'hello-world'
+  })
+
+  console.log(response)
+}
+
+test()
