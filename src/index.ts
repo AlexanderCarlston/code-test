1b71e25031f481a270092087b82df7c508460f26
AlexanderCarlston <alekscarlston@gmail.com>
2021-07-28 16:01:31 -0600

    Add basic functionality, blocked by rate limit

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,48 @@
 import express from "express";
+import { Octokit } from "@octokit/core";
+import { Endpoints } from "@octokit/types";
 const app = express();
-const port = 8080; // default port to listen
-const { Octokit } = require("@octokit/core");
+const port = 8080;
 const octokit = new Octokit();
 
-// define a route handler for the default home page
+type listUserRepoPullsParameters = Endpoints["GET /repos/{owner}/{repo}/pulls"]["parameters"];
+type listUserRepoPullsResponse = Endpoints["GET /repos/{owner}/{repo}"]["response"];
+
 app.get( "/", ( req, res ) => {
     res.send( "Hello world!" );
-} );
+});
 
-// start the Express server
 app.listen( port, () => {
+    // tslint:disable-next-line:no-console
     console.log( `server started at http://localhost:${ port }` );
-} );
+});
 
 
-async function test() {
+async function getOpenPullRequests(owner: listUserRepoPullsParameters["owner"], repo: listUserRepoPullsParameters["repo"]) {
   const response = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
-    owner: 'octocat',
-    repo: 'hello-world'
+    owner,
+    repo,
+    state: 'open'
+  })
+  // tslint:disable-next-line:no-console
+  console.log('response', response.data)
+
+  response.data.forEach(pullRequest => {
+     getCommitList(owner, repo, pullRequest.number)
+  })
+}
+
+// tslint:disable-next-line:variable-name
+async function getCommitList(owner: string, repo: string, pull_number: number) {
+  const response = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/commits', {
+    owner,
+    repo,
+    pull_number,
   })
 
+  // tslint:disable-next-line:no-console
   console.log(response)
 }
 
-test()
+// tslint:disable-next-line:no-console
+console.log(getOpenPullRequests('AlexanderCarlston', 'fool-project'))
